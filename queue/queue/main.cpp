adb4489f0cad31f2b9aceb6d0b14bc32cb7fcfbb
jiwook021 <jiwook021@gmail.com>
2022-08-09 23:57:12 -0700

    edit

--- queue/queue/main.cpp
+++ queue/queue/main.cpp
@@ -6,19 +6,12 @@ int main()
 	queue test; 
 	test.enqueue(1);
 	std::cout << test.peek() << std::endl; 
-
 	test.enqueue(2);
-
 	std::cout << test.peek() << std::endl;
-
 	test.enqueue(3);
-
 	std::cout << test.peek() << std::endl;
-
 	test.enqueue(4);
-
 	std::cout << test.peek() << std::endl;
-
 	test.enqueue(5);
 	
 	test.dequeue(); 
